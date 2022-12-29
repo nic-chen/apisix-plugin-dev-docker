c40303964a6aba56380ab2456de642a4ecf51a0e
nic-chen <chenjunxu6@gmail.com>
2022-12-29 18:43:45 +0800

    fix error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,32 +13,18 @@ fi
 
 cp -r /opt/custom-project/. /opt/apisix
 
-if [[ -d /opt/apisix ]];then
-    echo "Found apisix source code in /opt/apisix, make init, Plz Wait..."
-    cd /opt/apisix \
-    && make deps \
-    && make init \
-    && printf "
+
+echo "Found apisix source code in /opt/apisix, make init, Plz Wait..."
+cd /opt/apisix \
+&& make deps \
+&& make init \
+&& printf "
 init success.
 
 current you can:
 
 run \`prove -I/opt/test-nginx/lib -r /opt/apisix/t/plugin/<xxx.t>\` to run a specified test case;
 run \`prove -I/opt/test-nginx/lib -r /opt/apisix/t .\` to run all test case
-" 
-
-else
-    printf "
-Can not foud apisix source code in /opt/apisix, so you need run these commands to init apisix:
-
-cd /path/to/apisix/
-make deps
-make init
-
-After, You can:
-run \`prove -I/opt/test-nginx/lib -r /path/to/apisix/t/plugin/<xxx.t>\` to run a specified test case;
-run \`prove -I/opt/test-nginx/lib -r /path/to/apisix/t .\` to run all test case
 "
-fi
 
 exec "$@"
